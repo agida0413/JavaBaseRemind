d5c90000db38f9d57d0048b12a11dfd15adaa87c
agida0413 <144654789+agida0413@users.noreply.github.com>
2024-08-17 10:04:39 +0900

    variable

---
+++ Variable/src/variable/Main.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Variable var= new Variable();
+		
+		System.out.println(var.value1);//초기화x ===> 0 
+		System.out.println(var.value2);//초기화
+	}
+}
