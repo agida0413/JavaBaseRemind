b0e46e301674ad3735369abbd4dc78c3b38f3487
agida0413 <144654789+agida0413@users.noreply.github.com>
2024-08-28 23:25:56 +0900

    static

---
+++ Static/src/com/Data.java
@@ -0,0 +1,12 @@
+package com;
+
+public class Data {
+
+	public String name;
+	public static int count;
+	
+	public Data(String name) {
+		this.name=name;
+		count++;
+	}
+}
---
+++ Static/src/com/Main.java
@@ -0,0 +1,13 @@
+package com;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Data data1= new Data("김");
+		Data data2= new Data("용");
+		Data data3= new Data("준");
+		
+		System.out.println(Data.count);//count=3 클래스 명으로 접근 
+	}
+}
